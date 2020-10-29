1d4bf0930095bc1349aeac177e08e343c39d7167
TwiKx <aubin.coste@gmail.com>
2020-10-29 13:03:14 +0100

    add number to values string

--- src/service/RandomInRange.java
+++ src/service/RandomInRange.java
@@ -14,13 +14,13 @@ public class RandomInRange {
                 Random r = new Random();
                 int newEntry = r.nextInt((1000 - (-1000)) + 1 ) - 1000;
                 if (newEntry % 3 == 0 && newEntry % 5 == 0){
-                    this.listStrg.add("Gestform");
+                    this.listStrg.add("Gestform : " + Integer.toString(newEntry));
                 }
                 else if (newEntry % 3 == 0){
-                    this.listStrg.add("Geste");
+                    this.listStrg.add("Geste : " + Integer.toString(newEntry));
                 }
                 else if (newEntry % 5 == 0){
-                    this.listStrg.add("Forme");
+                    this.listStrg.add("Forme : " + Integer.toString(newEntry));
                 }
                 else {
                     this.listStrg.add(Integer.toString(newEntry));
